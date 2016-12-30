5f26f69ec021c768f9a5ef5b98dbeb14613923df
unknown <>
2016-12-30 11:34:29 +0800

    随便试试

---
+++ alarm/itemorderwave/querysalesman/main.js
@@ -0,0 +1,29 @@
+var $ = require('jquery')
+var Table = require('table-alarm')
+
+$( document ).ready( function(){	//初始化时间组件
+	$('#month-box').datetimepicker({
+		weekStart: 1,
+		autoclose: 1,
+		pickerPosition: 'bottom-left',
+		startView: 3,
+		minView: 3,
+		forceParse: 0
+	})
+	var monthId =$('#month-id').val()
+	var year = ''
+	var month = ''
+	if (monthId == ''){
+		var myDate = new Date()
+		myDate.setMonth(myDate.getMonth() - 1)
+		year = myDate.getFullYear()
+		month = myDate.getMonth() + 1
+	} else {
+		year = monthId.substring(0,4)
+		month = monthId.substring(4,6)
+	}
+	$('#month-box').datetimepicker('update', year + '-' + month)
+	Table.init()
+	}
+)
+
